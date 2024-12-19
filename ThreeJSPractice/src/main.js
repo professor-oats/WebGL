d04cc995994ae2149d5c190a7ea00ca0f25a2bd4
professor-oats <chronophob@gmail.com>
2024-12-19 17:57:31 +0100

    added anim loop and window resize update

--- ThreeJSPractice/src/main.js
+++ ThreeJSPractice/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import "./style.css"
 
 // Scene
 const scene = new THREE.Scene();
@@ -13,7 +14,13 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh);
 
-// light
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+// Light
 const light = new THREE.PointLight(0xffffff, 200, 100);
 light.position.set(0, 10, 11);
 scene.add(light);
@@ -21,7 +28,9 @@ scene.add(light);
 // Camera
 const camera = new THREE.PerspectiveCamera(
   45,
-  800/600,
+  sizes.width/sizes.height,
+  0.1,
+  100,
 )
 camera.position.z = 20;
 scene.add(camera);
@@ -29,7 +38,28 @@ scene.add(camera);
 // Renderer
 const canvas = document.getElementById('canvas');
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 //renderer.physicallyCorrectLights = true; // For accurate lighting calculations
-renderer.render(scene, camera);
+//renderer.render(scene, camera);
+
+// Animation Loop
+function animate() {
+  requestAnimationFrame(animate); // Schedule the next frame
+  renderer.render(scene, camera); // Render the scene
+}
+
+animate()
+
+
+// Resize - So we can resize the scene to render when we resize the window
+window.addEventListener('resize', () => {
+  // Update Sizes
+  console.log(window.innerWidth)
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  // Update Camera
+  camera.aspect = sizes.width/sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+})
 
