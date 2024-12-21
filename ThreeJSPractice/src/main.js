b06c9c3b0c2ecc0a3fe389cd698abab258d24a3d
professor-oats <chronophob@gmail.com>
2024-12-21 15:18:12 +0100

    el finito

--- ThreeJSPractice/src/main.js
+++ ThreeJSPractice/src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import "./style.css"
+import gsap from "gsap";
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
 /* Note: We shall look more into the three/examples library
@@ -54,20 +55,51 @@ const canvas = document.getElementById('canvas');
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 //renderer.physicallyCorrectLights = true; // For accurate lighting calculations
-//renderer.render(scene, camera);
+renderer.setPixelRatio(2);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate =true;
+controls.autoRotateSpeed = 5;
 
 // Animation Loop
 function animate() {
+  controls.update()
   window.requestAnimationFrame(animate); // Schedule the next frame
   renderer.render(scene, camera); // Render the scene
 }
 
 animate()
 
+// Timeline magiccc
+const tl = gsap.timeline({defaults: {duration: 1} });
+tl.fromTo(mesh.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1});
+tl.fromTo('nav', {y: '-100%'}, {y: '0%'});
+tl.fromTo('.title', {opacity: 0}, {opacity: 1});
+
+// Mouse Animation Colorrrr
+let mouseDown = false;
+let rgb = []
+
+
+window.addEventListener('mousedown', () => (mouseDown = true));
+window.addEventListener('mouseup', () => (mouseDown = false));
+window.addEventListener('mousemove', (e) => {
+  if(mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255), // Gives a color range when
+      // dragging mouse over the x-axis
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ]
+    // Let's animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(',')})`);
+    gsap.to(mesh.material.color, {r: newColor.r, g: newColor.g, b: newColor.b});
+  }
+});
 
 // Resize - So we can resize the scene to render when we resize the window
 window.addEventListener('resize', () => {
